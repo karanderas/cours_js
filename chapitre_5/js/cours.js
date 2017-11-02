1e24171978c5c03c1b0f9d83f56b1ebe359254c7
root <root@GPSA-D27PRH2.gpsa.fr>
2017-11-02 17:17:01 +0100

    2nd commit

--- chapitre_5/js/cours.js
+++ chapitre_5/js/cours.js
@@ -1,7 +1,15 @@
+// fonction public ------------------
 function clic() {
     console.log("Clic !");
 }
-
 var boutonElt = document.getElementById("bouton");
 // Ajout d'un gestionnaire pour l'événement click
 boutonElt.addEventListener("click", clic);
+
+
+//fonction anonyme
+var boutonElt = document.getElementById("bouton");
+// Ajout d'un gestionnaire pour l'événement click
+boutonElt.addEventListener("click", function () {
+    console.log("clic");
+});
