510ad7c7955309012d47c606d85a4eb09bd6af8a
root <root@GPSA-D27PRH2.gpsa.fr>
2017-11-02 17:53:12 +0100

    4ème commit

--- chapitre_5/js/cours.js
+++ chapitre_5/js/cours.js
@@ -13,4 +13,56 @@ var boutonElt = document.getElementById("bouton");
 boutonElt.addEventListener("click", function () {
     console.log("clic");
 });
-//end
+
+
+// Suppression du gestionnaire pour l'événement click
+boutonElt.removeEventListener("click", clic);
+
+
+// Ajout d'un gestionnaire qui affiche le type et la cible de l'événement
+document.getElementById("bouton").addEventListener("click", function (e) {
+    console.log("Evènement : " + e.type + 
+        ", texte de la cible : " + e.target.textContent);
+});
+
+
+// Gestion de l'appui sur une touche du clavier produisant un caractère
+document.addEventListener("keypress", function (e) {
+    console.log("Vous avez appuyé sur la touche " + String.fromCharCode(e.charCode));
+});
+
+// Affiche des informations sur un événement clavier
+function infosClavier(e) {
+    console.log("Evènement clavier : " + e.type + ", touche : " + e.keyCode);
+}
+
+// Gestion de l'appui et du relâchement d'une touche du clavier
+document.addEventListener("keydown", infosClavier);
+document.addEventListener("keyup", infosClavier);
+
+
+// Renvoie le nom du bouton souris à partir de son code
+function getBoutonSouris(code) {
+    var bouton = "inconnu";
+    switch (code) {
+    case 0: // 0 est le code du bouton gauche
+        bouton = "gauche";
+        break;
+    case 1: // 1 est le code du bouton du milieu
+        bouton = "milieu";
+        break;
+    case 2: // 2 est le code du bouton droit
+        bouton = "droit";
+        break;
+    }
+    return bouton;
+}
+
+// Affiche des informations sur un événement souris
+function infosSouris(e) {
+    console.log("Evènement souris : " + e.type + ", bouton " +
+        getBoutonSouris(e.button) + ", X : " + e.clientX + ", Y : " + e.clientY);
+}
+
+// Gestion du clic souris
+document.addEventListener("click", infosSouris);
